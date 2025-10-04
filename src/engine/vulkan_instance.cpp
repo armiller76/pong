31120d315db7f5afc765d5353bdedf92ce0aafd6
Andy Miller <armiller76@gmail.com>
2025-10-04 10:03:06 -0400

    Remove app and engine names from Instance class

--- src/engine/vulkan_instance.cpp
+++ src/engine/vulkan_instance.cpp
@@ -5,14 +5,12 @@ namespace pong
 
 VulkanInstance::VulkanInstance(
     const ::vk::raii::Context &context,
-    std::string application_name,
-    std::string engine_name,
+    std::string_view application_name,
+    std::string_view engine_name,
     uint32_t major_version,
     uint32_t minor_version,
     uint32_t patch_version)
-    : application_name_(std::move(application_name))
-    , engine_name_(std::move(engine_name))
-    , instance_(
+    : instance_(
           context,
           [=]
           {
@@ -20,8 +18,8 @@ VulkanInstance::VulkanInstance(
               vk_application_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
               vk_application_info.apiVersion = VK_API_VERSION_1_3;
               vk_application_info.applicationVersion = VK_MAKE_VERSION(major_version, minor_version, patch_version);
-              vk_application_info.pApplicationName = application_name_.c_str();
-              vk_application_info.pEngineName = engine_name_.c_str();
+              vk_application_info.pApplicationName = application_name.data();
+              vk_application_info.pEngineName = engine_name.data();
 
               auto vk_instance_create_info = VkInstanceCreateInfo{};
               vk_instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -35,16 +33,6 @@ VulkanInstance::VulkanInstance(
 auto VulkanInstance::get() const -> const ::vk::raii::Instance &
 {
     return instance_;
-}
-
-auto VulkanInstance::application_name() const -> std::string_view
-{
-    return application_name_;
-}
-
-auto VulkanInstance::engine_name() const -> std::string_view
-{
-    return engine_name_;
 }
 
 } // namespace pong
--- src/engine/vulkan_instance.h
+++ src/engine/vulkan_instance.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cstdint>
-#include <string>
 #include <string_view>
 
 #include <vulkan/vulkan_raii.hpp>
@@ -13,19 +12,15 @@ class VulkanInstance
   public:
     VulkanInstance(
         const ::vk::raii::Context &context,
-        std::string application_name,
-        std::string engine_name,
+        std::string_view application_name,
+        std::string_view engine_name,
         uint32_t major_version,
         uint32_t minor_version,
         uint32_t patch_version);
 
     auto get() const -> const ::vk::raii::Instance &;
-    auto application_name() const -> std::string_view;
-    auto engine_name() const -> std::string_view;
 
   private:
-    std::string application_name_;
-    std::string engine_name_;
     ::vk::raii::Instance instance_;
 };
 
