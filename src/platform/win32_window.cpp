c6df1594c9d6120908e386bcca3dc7be806d83df
Andy Miller <armiller76@gmail.com>
2025-10-04 19:02:43 -0400

    Tidy up unneeded debug code

--- src/platform/win32_window.cpp
+++ src/platform/win32_window.cpp
@@ -26,7 +26,6 @@ Win32Window::Win32Window(std::string_view app_name, Win32WindowCreateInfo create
         throw arm::Exception("Failed to create window class");
     }
 
-    ::SetLastError(0);
     window_ = {
         ::CreateWindowExA(
             WS_EX_OVERLAPPEDWINDOW,
@@ -42,13 +41,8 @@ Win32Window::Win32Window(std::string_view app_name, Win32WindowCreateInfo create
             win32_instance,
             this),
         ::DestroyWindow};
-    auto err = ::GetLastError();
-    char buf[256];
-    ::FormatMessageA(
-        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, err, 0, buf, sizeof(buf), nullptr);
-    arm::log::error("{}", buf);
-    ::ShowWindow(window_, SW_SHOWNORMAL);
 
+    ::ShowWindow(window_, SW_SHOWNORMAL);
     running_ = true;
 }
 
@@ -72,21 +66,14 @@ auto Win32Window::running() const -> bool
 
 auto CALLBACK Win32Window::instance_window_callback(HWND window, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT
 {
-    // get a pointer to the actual Win32Window instance so we can call into the actual event handler
     Win32Window *self = reinterpret_cast<Win32Window *>(::GetWindowLongPtrA(window, GWLP_USERDATA));
-
-    // if the pointer exists, we already know where the window instance is and can call into its event handler
     if (self)
     {
         return self->handle_message(window, msg, wParam, lParam);
     }
 
-    // if the pointer doesn't exist, and the message indicates a new window is being created, store the
-    // pointer
     if (msg == WM_NCCREATE)
     {
-        // windows will send us the window creation info in lParam, and we will have stored our pointer in its
-        // lpParam when we called CreateWindowEx and passed it 'this'
         auto create_struct = reinterpret_cast<CREATESTRUCT *>(lParam);
         auto this_pointer = reinterpret_cast<LONG_PTR>(create_struct->lpCreateParams);
         SetWindowLongPtr(window, GWLP_USERDATA, this_pointer);
