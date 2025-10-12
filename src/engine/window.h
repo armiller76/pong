8b4d206e6c0ae468372f24d5fb8f86919d952866
Andy Miller <armiller76@gmail.com>
2025-10-12 19:23:08 -0400

    Add Window base class

---
+++ src/engine/window.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <string_view>
+
+#include "utils/util.h"
+
+namespace pong
+{
+class VulkanInstance;
+class VulkanSurface;
+
+class Window
+{
+  public:
+    virtual ~Window() = default;
+
+    virtual auto process_events() -> void = 0;
+    virtual auto should_close() const -> bool = 0;
+
+    virtual auto size_pixels() const -> Size = 0;
+    virtual auto set_title(std::string_view title) -> void = 0;
+
+    virtual auto create_vulkan_surface(const VulkanInstance &instance) const -> VulkanSurface = 0;
+
+    virtual auto add_close_callback(std::function<void()>) -> std::uint64_t = 0;
+    virtual auto remove_close_callback(std::uint64_t) -> void = 0;
+
+    virtual auto add_resize_callback(std::function<void(std::uint32_t, std::uint32_t)>) -> std::uint64_t = 0;
+    virtual auto remove_resize_callback(std::uint64_t) -> void = 0;
+};
+
+}
