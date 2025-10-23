3b99f09718427942419d8721ccf8b6280e500df4
Andy Miller <armiller76@gmail.com>
2025-10-23 16:24:29 -0400

    Add Mesh class

---
+++ src/graphics/mesh.cpp
@@ -0,0 +1,67 @@
+#include "mesh.h"
+
+#include <array>
+#include <span>
+
+#include "glm_wrapper.h"
+
+#include "engine/vulkan/gpu_buffer.h"
+#include "graphics/vertex.h"
+#include "utils/error.h"
+
+namespace pong
+{
+
+Mesh::Mesh(const VulkanDevice &device, std::span<const Vertex> vertices)
+    : vertex_count_(vertices.size())
+    , vertex_buffer_(
+          device,
+          vertices.size_bytes(),
+          ::vk::BufferUsageFlagBits::eVertexBuffer,
+          ::vk::MemoryPropertyFlagBits::eHostCoherent | ::vk::MemoryPropertyFlagBits::eHostVisible)
+{
+    arm::ensure(!vertices.empty(), "cannot create Mesh with no vertices");
+    arm::ensure(vertices.size() <= UINT32_MAX, "too many vertices (uint32_t max)");
+
+    vertex_buffer_.upload(vertices.data(), vertices.size_bytes());
+
+    arm::log::debug("Created Mesh: vertices={} bytes={}", vertex_count_, vertices.size_bytes());
+}
+
+auto Mesh::vertex_count() const noexcept -> std::uint32_t
+{
+    return vertex_count_;
+}
+
+auto Mesh::vertex_buffer() const noexcept -> const GpuBuffer &
+{
+    return vertex_buffer_;
+}
+
+auto Mesh::size_bytes() const noexcept -> std::size_t
+{
+    return vertex_count_ * sizeof(Vertex);
+}
+
+auto Mesh::create_test_triangle(const VulkanDevice &device) -> Mesh
+{
+    constexpr auto vertices = std::array<Vertex, 3>{
+        {{.position = {0.0f, -0.5f}, .color = {1.0f, 0.0f, 0.0f}},
+         {.position = {0.5f, 0.5f}, .color = {0.0f, 1.0f, 0.0f}},
+         {.position = {-0.5f, 0.5f}, .color = {0.0f, 0.0f, 1.0f}}}};
+    return Mesh(device, vertices);
+}
+
+auto Mesh::create_test_rectangle(const VulkanDevice &device) -> Mesh
+{
+    constexpr auto vertices = std::array<Vertex, 6>{
+        {{.position = {-0.5f, -0.5f}, .color = {1.0f, 1.0f, 1.0f}},
+         {.position = {0.5f, -0.5f}, .color = {1.0f, 1.0f, 1.0f}},
+         {.position = {0.5f, 0.5f}, .color = {1.0f, 1.0f, 1.0f}},
+         {.position = {0.5f, 0.5f}, .color = {1.0f, 1.0f, 1.0f}},
+         {.position = {-0.5f, 0.5f}, .color = {1.0f, 1.0f, 1.0f}},
+         {.position = {-0.5f, -0.5f}, .color = {1.0f, 1.0f, 1.0f}}}};
+    return Mesh(device, vertices);
+}
+
+} // namespace pong
---
+++ src/graphics/mesh.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <span>
+
+#include "engine/vulkan/gpu_buffer.h"
+#include "utils/log.h"
+#include "vertex.h"
+
+namespace pong
+{
+class VulkanDevice;
+
+class Mesh
+{
+  public:
+    Mesh(const VulkanDevice &device, std::span<const Vertex> vertices);
+    ~Mesh() = default;
+
+    Mesh(Mesh &&) noexcept = default;
+    Mesh &operator=(Mesh &&) noexcept = default;
+    Mesh(const Mesh &) = delete;
+    Mesh &operator=(Mesh &) = delete;
+
+    auto vertex_count() const noexcept -> std::uint32_t;
+    auto vertex_buffer() const noexcept -> const GpuBuffer &;
+    auto size_bytes() const noexcept -> std::size_t;
+
+    static auto create_test_triangle(const VulkanDevice &device) -> Mesh;
+    static auto create_test_rectangle(const VulkanDevice &device) -> Mesh;
+
+  private:
+    std::uint32_t vertex_count_{0};
+    GpuBuffer vertex_buffer_;
+}; // class Vertex
+
+} // namespace pong
