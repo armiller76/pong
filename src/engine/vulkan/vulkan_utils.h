5ef392dd601fc865768949a0aa1b48f30d8464c9
Andy Miller <armiller76@gmail.com>
2025-10-28 17:51:57 -0400

    Add Vulkan utility header for various helpers

---
+++ src/engine/vulkan/vulkan_utils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vulkan/vulkan_raii.hpp>
+
+#include "graphics/shader.h"
+#include "utils/exception.h"
+
+namespace pong
+{
+
+inline auto to_vk_stage(ShaderStage stage) -> ::vk::ShaderStageFlagBits
+{
+    using enum ShaderStage;
+    switch (stage)
+    {
+        case Vertex: return ::vk::ShaderStageFlagBits::eVertex;
+        case Fragment: return ::vk::ShaderStageFlagBits::eFragment;
+        case Compute: return ::vk::ShaderStageFlagBits::eCompute;
+        default: throw arm::Exception("unknown shader stage");
+    }
+}
+
+[[nodiscard]] inline auto create_vk_shader_module(const vk::raii::Device &device, const Shader &shader)
+    -> ::vk::raii::ShaderModule
+{
+    return device.createShaderModule(
+        {{}, shader.spirv.size(), reinterpret_cast<const uint32_t *>(shader.spirv.data())});
+}
+
+}
