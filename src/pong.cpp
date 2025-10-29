72c8d026959bda2870f1e6b878fb067f05c61dad
Andy Miller <armiller76@gmail.com>
2025-10-29 15:25:21 -0400

    "Integrate" ResourceManager

--- src/pong.cpp
+++ src/pong.cpp
@@ -1,14 +1,17 @@
 #include <cstdlib>
+#include <filesystem>
 #include <print>
 #include <string>
 
 #include <vulkan/vulkan_raii.hpp>
 
+#include "engine/resource_manager.h"
 #include "engine/vulkan/vulkan_command_context.h"
 #include "engine/vulkan/vulkan_device.h"
 #include "engine/vulkan/vulkan_instance.h"
 #include "engine/vulkan/vulkan_surface.h"
 #include "engine/vulkan/vulkan_swapchain.h"
+#include "graphics/shader.h"
 #include "platform/win32_window.h"
 #include "utils/exception.h"
 #include "utils/log.h"
@@ -16,6 +19,7 @@
 
 int main()
 {
+    std::string project_root{"c:/dev/Pong"};
     std::string app_name{"Pong"};
     std::string engine_name{"NotAnEngine"};
 
@@ -39,8 +43,9 @@ int main()
         const auto vk_surface = window.create_vulkan_surface(vk_instance);
         const auto vk_device = pong::VulkanDevice(vk_instance, vk_surface);
 
-        // auto vk_swapchain = VulkanSwapchain(vk_surface, vk_device);
-        // auto vk_command_context = VulkanCommandContext(vk_device, 2u);
+        auto file = std::filesystem::path(project_root + "/assets/shaders/src/simple.vert");
+        auto resources = pong::ResourceManager(vk_device);
+        resources.load("simple.vert", file, pong::ShaderStage::Vertex);
 
         while (!window.should_close())
         {
