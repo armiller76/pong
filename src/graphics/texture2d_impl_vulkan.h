9d097d077753f2420303fe1b3aaa08aa76cb1cb3
Andy Miller <armiller76@gmail.com>
2026-04-13 15:25:11 -0400

    Make header-only

--- src/graphics/texture2d_impl_vulkan.h
+++ src/graphics/texture2d_impl_vulkan.h
@@ -13,12 +13,19 @@ namespace pong
 
 {
 
+class Image;
 class VulkanDevice;
+class VulkanImmediateCommandContext;
 
 class Texture2DImpl_Vulkan
 {
   public:
-    Texture2DImpl_Vulkan(const VulkanDevice &device, std::string_view name, ::vk::Extent2D extent, ::vk::Format format);
+    Texture2DImpl_Vulkan(const VulkanDevice &device, std::string_view name, ::vk::Extent2D extent, ::vk::Format format)
+        : name_{name}
+        , extent_{extent}
+        , image_{device, extent_, format}
+    {
+    }
     ~Texture2DImpl_Vulkan() = default;
 
     Texture2DImpl_Vulkan(const Texture2DImpl_Vulkan &) = delete;
@@ -26,13 +33,40 @@ class Texture2DImpl_Vulkan
     Texture2DImpl_Vulkan(Texture2DImpl_Vulkan &&) noexcept = default;
     auto operator=(Texture2DImpl_Vulkan &&) noexcept -> Texture2DImpl_Vulkan & = default;
 
-    auto name() const -> std::string_view;
-    auto width() const -> std::uint32_t;
-    auto height() const -> std::uint32_t;
+    auto name() const -> std::string_view
+    {
+        return name_;
+    }
 
-    auto image() const -> ::vk::Image;
-    auto image_view() const -> ::vk::ImageView;
-    auto sampler() const -> ::vk::Sampler;
+    auto width() const -> std::uint32_t
+    {
+        return extent_.width;
+    }
+
+    auto height() const -> std::uint32_t
+    {
+        return extent_.height;
+    }
+
+    auto image() const -> ::vk::Image
+    {
+        return image_.image();
+    }
+
+    auto image_view() const -> ::vk::ImageView
+    {
+        return image_.image_view();
+    }
+
+    auto sampler() const -> ::vk::Sampler
+    {
+        return image_.sampler();
+    }
+
+    auto upload(VulkanImmediateCommandContext &command_context, const Image &image)
+    {
+        image_.upload(command_context, image);
+    }
 
   private:
     std::string name_;
