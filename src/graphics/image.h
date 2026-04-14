6af7d75381c1f44b45927de4e6882ebcd8045e65
Andy Miller <armiller76@gmail.com>
2026-04-13 23:37:02 -0400

    Rearrange for visibility

--- src/graphics/image.h
+++ src/graphics/image.h
@@ -38,48 +38,6 @@ enum class ImageFormat
     UNDEFINED,
 };
 
-class Image
-{
-  public:
-    Image(std::string_view name, Extent2D extent, ImageFormat(format), std::span<const std::uint8_t> pixels)
-        : name_{name}
-        , extent_{extent}
-        , format_{format}
-        , pixels_{pixels.begin(), pixels.end()}
-    {
-        arm::log::debug("Image constructor {}", name);
-        arm::ensure(!(extent_.width == 0 || extent_.height == 0), "Image created with zero height or width");
-        arm::ensure(!(format_ == ImageFormat::UNDEFINED), "Unknown image format");
-        arm::ensure(!pixels_.empty(), "Image created with no pixels");
-    }
-
-    auto name() const -> std::string_view
-    {
-        return name_;
-    }
-
-    auto extent() const -> Extent2D
-    {
-        return extent_;
-    }
-
-    auto format() const -> ImageFormat
-    {
-        return format_;
-    }
-
-    auto pixels() const -> std::span<const std::uint8_t>
-    {
-        return pixels_;
-    }
-
-  private:
-    std::string name_;
-    Extent2D extent_;
-    ImageFormat format_;
-    std::vector<std::uint8_t> pixels_;
-}; // class Image
-
 inline auto bytes_per_pixel(ImageFormat f) -> std::uint32_t
 {
     using enum ImageFormat;
@@ -116,6 +74,51 @@ inline auto bytes_per_pixel(ImageFormat f) -> std::uint32_t
         default: throw arm::Exception("undefined ImageFormat in bytes_per_pixel");
     }
 }
+
+class Image
+{
+  public:
+    Image(std::string_view name, Extent2D extent, ImageFormat(format), std::span<const std::uint8_t> pixels)
+        : name_{name}
+        , extent_{extent}
+        , format_{format}
+        , pixels_{pixels.begin(), pixels.end()}
+    {
+        arm::log::debug("Image constructor {}", name);
+        arm::ensure(!(extent_.width == 0 || extent_.height == 0), "Image created with zero height or width");
+        arm::ensure(!(format_ == ImageFormat::UNDEFINED), "Unknown image format");
+        arm::ensure(!pixels_.empty(), "Image created with no pixels");
+        arm::ensure(
+            static_cast<std::size_t>(extent_.width) * extent_.height * bytes_per_pixel(format_) == pixels.size(),
+            "Extent width * height does not equal pixel byte count");
+    }
+
+    auto name() const -> std::string_view
+    {
+        return name_;
+    }
+
+    auto extent() const -> Extent2D
+    {
+        return extent_;
+    }
+
+    auto format() const -> ImageFormat
+    {
+        return format_;
+    }
+
+    auto pixels() const -> std::span<const std::uint8_t>
+    {
+        return pixels_;
+    }
+
+  private:
+    std::string name_;
+    Extent2D extent_;
+    ImageFormat format_;
+    std::vector<std::uint8_t> pixels_;
+}; // class Image
 
 inline auto to_string(ImageFormat f) -> std::string_view
 {
