e04e760516d1a4a0cc7f8be51b7093a98dc6c150
Andy Miller <armiller76@gmail.com>
2026-04-14 11:48:44 -0400

    Split ImageFormat to own file

--- src/graphics/image.h
+++ src/graphics/image.h
@@ -4,76 +4,13 @@
 #include <string>
 #include <string_view>
 
+#include "image_format.h"
 #include "math/rectangle.h"
 #include "utils/error.h"
 #include "utils/log.h"
 
 namespace pong
 {
-using namespace std::literals;
-
-enum class ImageFormat
-{
-    RGBA8,
-    RGB8,
-    BGRA8,
-    R8,
-    RG8,
-    RGBA16F,
-    RGBA32F,
-    RGBA8_sRGB,
-    D16,
-    D24,
-    D32F,
-    D24S8,
-    D32FS8,
-    BC1,
-    BC2,
-    BC3,
-    BC4,
-    BC5,
-    BC6H,
-    BC7,
-
-    UNDEFINED,
-};
-
-inline auto bytes_per_pixel(ImageFormat f) -> std::uint32_t
-{
-    using enum ImageFormat;
-    switch (f)
-    {
-        case R8: return 1u;
-
-        case RG8: return 2u;
-
-        case D24:
-        case RGB8: return 3u;
-
-        case D16:
-        case D24S8:
-        case BGRA8:
-        case RGBA8:
-        case RGBA8_sRGB: return 4u;
-
-        case D32FS8:
-        case RGBA16F: return 8u;
-
-        case D32F:
-        case RGBA32F: return 16u;
-
-        case BC1:
-        case BC2:
-        case BC3:
-        case BC4:
-        case BC5:
-        case BC6H:
-        case BC7: return 0u;
-
-        case UNDEFINED:
-        default: throw arm::Exception("undefined ImageFormat in bytes_per_pixel");
-    }
-}
 
 class Image
 {
@@ -119,36 +56,5 @@ class Image
     ImageFormat format_;
     std::vector<std::uint8_t> pixels_;
 }; // class Image
-
-inline auto to_string(ImageFormat f) -> std::string_view
-{
-    using enum ImageFormat;
-    switch (f)
-    {
-        case R8: return "R8"sv;
-        case RG8: return "RG8"sv;
-        case D24: return "D24"sv;
-        case RGB8: return "RGB8"sv;
-        case D16: return "D16"sv;
-        case D24S8: return "D24S8"sv;
-        case BGRA8: return "BGRA8"sv;
-        case RGBA8: return "RGBA8"sv;
-        case RGBA8_sRGB: return "RGBA8_sRGB"sv;
-        case D32FS8: return "D32FS8"sv;
-        case RGBA16F: return "RBGA16F"sv;
-        case D32F: return "D32F"sv;
-        case RGBA32F: return "RGBA32F"sv;
-        case BC1: return "BC1"sv;
-        case BC2: return "BC2"sv;
-        case BC3: return "BC3"sv;
-        case BC4: return "BC4"sv;
-        case BC5: return "BC5"sv;
-        case BC6H: return "BC6H"sv;
-        case BC7: return "BC7"sv;
-
-        case UNDEFINED: return "UNDEFINED"sv;
-        default: throw arm::Exception("undefined ImageFormat in to_string");
-    }
-}
 
 } // namespace pong
---
+++ src/graphics/image_format.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <cstdint>
+#include <string_view>
+
+#include "utils/exception.h"
+
+namespace pong
+{
+
+using namespace std::literals;
+
+enum class ImageFormat
+{
+    RGBA8,
+    RGB8,
+    BGRA8,
+    R8,
+    RG8,
+    RGBA16F,
+    RGBA32F,
+    RGBA8_sRGB,
+    D16,
+    D24,
+    D32F,
+    D24S8,
+    D32FS8,
+    BC1,
+    BC2,
+    BC3,
+    BC4,
+    BC5,
+    BC6H,
+    BC7,
+
+    UNDEFINED,
+}; // enum class ImageFormat
+
+inline auto bytes_per_pixel(ImageFormat f) -> std::uint32_t
+{
+    using enum ImageFormat;
+    switch (f)
+    {
+        case R8: return 1u;
+
+        case D16:
+        case RG8: return 2u;
+
+        case RGB8: return 3u;
+
+        case D24:
+        case D24S8:
+        case D32F:
+        case BGRA8:
+        case RGBA8:
+        case RGBA8_sRGB: return 4u;
+
+        case D32FS8:
+        case RGBA16F: return 8u;
+
+        case RGBA32F: return 16u;
+
+        case BC1:
+        case BC2:
+        case BC3:
+        case BC4:
+        case BC5:
+        case BC6H:
+        case BC7: return 0u;
+
+        case UNDEFINED:
+        default: throw arm::Exception("undefined ImageFormat in bytes_per_pixel");
+    }
+}
+
+inline auto to_string(ImageFormat f) -> std::string_view
+{
+    using enum ImageFormat;
+    switch (f)
+    {
+        case R8: return "R8"sv;
+        case RG8: return "RG8"sv;
+        case D24: return "D24"sv;
+        case RGB8: return "RGB8"sv;
+        case D16: return "D16"sv;
+        case D24S8: return "D24S8"sv;
+        case BGRA8: return "BGRA8"sv;
+        case RGBA8: return "RGBA8"sv;
+        case RGBA8_sRGB: return "RGBA8_sRGB"sv;
+        case D32FS8: return "D32FS8"sv;
+        case RGBA16F: return "RGBA16F"sv;
+        case D32F: return "D32F"sv;
+        case RGBA32F: return "RGBA32F"sv;
+        case BC1: return "BC1"sv;
+        case BC2: return "BC2"sv;
+        case BC3: return "BC3"sv;
+        case BC4: return "BC4"sv;
+        case BC5: return "BC5"sv;
+        case BC6H: return "BC6H"sv;
+        case BC7: return "BC7"sv;
+
+        case UNDEFINED: return "UNDEFINED"sv;
+        default: throw arm::Exception("undefined ImageFormat in to_string");
+    }
+}
+
+} // namespace pong
