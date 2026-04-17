6bbb54300ae4b9933396c8b8f5c356fc3df13464
Andy Miller <armiller76@gmail.com>
2026-04-16 20:58:25 -0400

    Add Traits for Resource classes

---
+++ src/core/resource_traits.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "resource_handles.h"
+
+namespace pong
+{
+
+struct Material;
+class Mesh;
+struct Model;
+struct Shader;
+class Texture2D;
+
+template <typename T>
+struct ResourceTraits;
+
+template <>
+struct ResourceTraits<Material>
+{
+    using handle_type = MaterialHandle;
+};
+
+template <>
+struct ResourceTraits<Mesh>
+{
+    using handle_type = MeshHandle;
+};
+
+template <>
+struct ResourceTraits<Model>
+{
+    using handle_type = ModelHandle;
+};
+
+template <>
+struct ResourceTraits<Shader>
+{
+    using handle_type = ShaderHandle;
+};
+
+template <>
+struct ResourceTraits<Texture2D>
+{
+    using handle_type = Texture2DHandle;
+};
+
+} // namespace pong
