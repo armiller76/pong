d0fe860aba4279cc96c11afc03b3f91ec17aa5cd
Andy Miller <armiller76@gmail.com>
2026-04-18 02:01:53 -0400

    Renderer third pass refactor for render heirarchy

--- src/engine/resource_loader.cpp
+++ src/engine/resource_loader.cpp
@@ -2,11 +2,14 @@
 
 #include <cstdint>
 #include <filesystem>
+#include <format>
 #include <string_view>
 #include <vector>
 
+#include "core/entity.h"
 #include "engine/file.h"
 #include "engine/vulkan/vulkan_device.h"
+#include "graphics/glm_wrapper.h"
 #include "graphics/shader.h"
 #include "graphics/utils.h"
 #include "resource_manager.h"
@@ -29,6 +32,28 @@ ResourceLoader::ResourceLoader(
     arm::log::debug("ResourceLoader constructor");
 }
 
+auto ResourceLoader::loadgltf(std::filesystem::path path) -> std::vector<Entity>
+{
+    auto gltf = FastGLTFWrapper();
+    const auto loaded_asset = gltf.load(path);
+
+    // const auto scene_count = loaded_asset.scenes.size();
+    const auto default_scene_index = loaded_asset.default_scene_index.value_or(0);
+    const auto &default_scene = loaded_asset.scenes[default_scene_index];
+
+    arm::log::debug(
+        "gltf loaded:\n default scene index: {}\ndefault scene name: {}", default_scene_index, default_scene.name);
+    auto entities = std::vector<Entity>();
+    for (auto i = 0zu; i < default_scene.root_node_indices.size(); ++i)
+    {
+        const auto &node = loaded_asset.nodes[default_scene.root_node_indices[i]];
+        const auto parent_transform = ::glm::mat4x4(1.0f);
+        arm::log::debug("processing node {} with index #{}", node.name, default_scene.root_node_indices[i]);
+        process_loaded_node_(loaded_asset, node, entities, parent_transform);
+    }
+    return entities;
+}
+
 auto ResourceLoader::load(std::string_view name, std::filesystem::path path, ShaderStage stage) -> ShaderHandle
 {
     auto file = File(path);
@@ -48,34 +73,69 @@ auto ResourceLoader::load(std::string_view name, std::filesystem::path path, Sha
     return resource_manager_.insert<Shader>(name, std::move(shader));
 }
 
-auto ResourceLoader::load(std::string_view name, std::filesystem::path path) -> MeshHandle
-{
-    const auto key = MeshHandle{get_resource_id_(name)};
-
-    if (resource_manager_.contains<Mesh>(key))
-    {
-        arm::log::warn("mesh already loaded: {} (skipping)", name);
-        return key;
-    }
-
-    auto gltf = FastGLTFWrapper();
-    auto loaded_asset = gltf.load(path);
-
-    return resource_manager_.insert<Mesh>(
-        name,
-        Mesh{
-            device_,
-            loaded_asset.meshes[0].name,
-            loaded_asset.meshes[0].primitives[0].vertices,
-            loaded_asset.meshes[0].primitives[0].indices});
-}
-
 auto ResourceLoader::load(std::string_view name, Image &image) -> Texture2DHandle
 {
     auto texture = Texture2D{image, device_};
     texture.upload_pixels(command_context_, image);
 
     return resource_manager_.insert<Texture2D>(name, std::move(texture));
+}
+
+auto ResourceLoader::process_loaded_node_(
+    const LoadedAsset &asset,
+    const LoadedNode &node,
+    std::vector<Entity> &entities,
+    const ::glm::mat4x4 &parent_transform) -> void
+{
+    auto transform = parent_transform * ::glm::mat4(node.local_transform);
+    if (node.mesh_index.has_value())
+    {
+        // if mesh_index has a value, this is effectively an Entity.
+        // Get the transform from node, model from its mesh
+        arm::log::debug("  - node has mesh_index");
+        auto entity = Entity{};
+        auto mesh_index = node.mesh_index.value();
+        entity.set_name(node.name);
+        entity.set_transform(transform);
+        entity.model().name = asset.meshes[mesh_index].name;
+
+        const auto &primitives = asset.meshes[mesh_index].primitives;
+        arm::log::debug("  - node has {} primitives", primitives.size());
+
+        for (auto i = 0zu; i < primitives.size(); ++i)
+        {
+            auto name = std::format("{}_{}", entity.model().name, i);
+            auto mesh_handle =
+                resource_manager_.insert<Mesh>(name, {device_, name, primitives[i].vertices, primitives[i].indices});
+            arm::log::debug("    - added mesh {} {}", name, mesh_handle.value);
+
+            auto renderable = Renderable{};
+            if (primitives[i].material_index.has_value())
+            {
+                auto material_name = asset.materials[primitives[i].material_index.value()].name;
+                auto material_handle = resource_manager_.insert<Material>(material_name, {material_name});
+                renderable = Renderable{mesh_handle, std::make_optional(material_handle)};
+                arm::log::debug("    - with material {} {}", material_name, material_handle.value);
+            }
+            else
+            {
+                renderable = Renderable{mesh_handle, std::nullopt};
+                arm::log::debug("    - without material");
+            }
+            entity.model().renderables.push_back(std::move(renderable));
+        }
+        entities.push_back(std::move(entity));
+        arm::log::debug("Entity created with {} renderables", entity.model().renderables.size());
+    }
+    else
+    {
+        // TODO do i really need to do anything here?
+    }
+    for (auto i = 0zu; i < node.child_indices.size(); ++i)
+    {
+        arm::log::debug("Moving to child node, index #{}", node.child_indices[i]);
+        process_loaded_node_(asset, asset.nodes[node.child_indices[i]], entities, transform);
+    }
 }
 
 auto ResourceLoader::get_resource_id_(std::string_view name) -> std::uint64_t
--- src/engine/resource_loader.h
+++ src/engine/resource_loader.h
@@ -3,16 +3,18 @@
 #include <cstdint>
 #include <filesystem>
 #include <string_view>
-
+#include <vector>
 
 #include "core/resource_handles.h"
 #include "engine/vulkan/vulkan_immediate_command_context.h"
+#include "gltf/fastgltf_primitives.h"
 #include "gltf/fastgltf_wrapper.h"
 #include "resource_manager.h"
 
 namespace pong
 {
 
+class Entity;
 class Image;
 class VulkanDevice;
 enum class ShaderStage;
@@ -25,12 +27,10 @@ class ResourceLoader
         ResourceManager &resource_manager,
         std::filesystem::path absolute_path_to_assets);
 
+    auto loadgltf(std::filesystem::path path) -> std::vector<Entity>;
+
     // Shader loading
     auto load(std::string_view name, std::filesystem::path path, ShaderStage stage) -> ShaderHandle;
-
-    // Mesh loading
-    auto load(std::string_view name, std::filesystem::path path) -> MeshHandle;
-
     // Texture loading
     auto load(std::string_view name, Image &image) -> Texture2DHandle;
 
@@ -40,6 +40,11 @@ class ResourceLoader
     std::filesystem::path absolute_path_to_assets_;
     ResourceManager &resource_manager_;
 
+    auto process_loaded_node_(
+        const LoadedAsset &asset,
+        const LoadedNode &node,
+        std::vector<Entity> &entities,
+        const ::glm::mat4x4 &parent_transform) -> void;
     auto get_resource_id_(std::string_view name) -> std::uint64_t;
 }; // class ResourceLoader
 
