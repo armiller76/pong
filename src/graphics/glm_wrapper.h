cbf205b6e57267e693fcdd131e4bc2d6197ca5bc
Andy Miller <armiller76@gmail.com>
2026-04-17 22:00:55 -0400

    Add glm matrix decompose

--- src/graphics/glm_wrapper.h
+++ src/graphics/glm_wrapper.h
@@ -3,5 +3,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtx/string_cast.hpp>
+
