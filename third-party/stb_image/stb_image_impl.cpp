9c9309106fc4596c0d6cfd001f1517f0f05715a1
Andy Miller <armiller76@gmail.com>
2026-04-18 22:29:52 -0400

    Add stb_image image loader to project

---
+++ third-party/stb_image/stb_image_impl.cpp
@@ -0,0 +1,7 @@
+#pragma warning(push, 0)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weverything"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#pragma GCC diagnostic pop
+#pragma warning(pop)
