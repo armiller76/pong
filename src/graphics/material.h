89211e4d8a262a07f0491fa5eb5d04c25473cc58
Andy Miller <armiller76@gmail.com>
2026-04-18 22:30:56 -0400

    Expand Material struct to hold actual material data

--- src/graphics/material.h
+++ src/graphics/material.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <optional>
 #include <string>
+
+#include "core/resource_handles.h"
+#include "graphics/glm_wrapper.h" // IWYU pragma: keep
+#include "graphics/types.h"
 
 namespace pong
 {
@@ -8,6 +13,13 @@ namespace pong
 struct Material
 {
     std::string name;
+    ::glm::vec4 base_color_factor;
+    std::optional<Texture2DHandle> base_color_texture_handle;
+    float metallic_factor;
+    float roughness_factor;
+    std::optional<Texture2DHandle> metallic_roughness_texture_handle;
+    std::optional<Texture2DHandle> normal_texture_handle;
+    AlphaMode alpha_mode;
 }; // struct Material
 
 } // namespace pong
