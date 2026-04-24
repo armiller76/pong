d1d53c919ddaaa88dcfc849c5f77156ee11747d9
Andy Miller <armiller76@gmail.com>
2026-04-23 20:59:43 -0400

    Add RGB -> sRBG and float -> byte helpers

--- src/graphics/color.h
+++ src/graphics/color.h
@@ -1,10 +1,9 @@
 #pragma once
 
 #include <algorithm>
+#include <cstdint>
 #include <format>
 #include <string>
-
-#include "utils/log.h"
 
 namespace pong
 {
@@ -23,6 +22,30 @@ struct Color
         this->b = std::clamp(b, 0.0f, 1.0f);
         this->a = std::clamp(a, 0.0f, 1.0f);
     }
+
+    static constexpr auto float_to_byte(float f) -> std::uint8_t
+    {
+        return std::round(std::clamp<float>(f, 0.0f, 1.0f) * 255.0f);
+    }
+
+    static constexpr auto float_to_srgb(float f) -> float
+    {
+        auto result = float{};
+        if (f < 0.0031308f)
+        {
+            result = 12.92 * f;
+        }
+        else
+        {
+            result = 1.055 * std::pow(f, 1.0f / 2.4f) - 0.055;
+        }
+        return result;
+    }
+
+    static constexpr auto float_to_srgb_byte(float f) -> std::uint8_t
+    {
+        return float_to_byte(float_to_srgb(f));
+    }
 };
 
 inline auto to_string(const Color &color) -> std::string
@@ -30,4 +53,4 @@ inline auto to_string(const Color &color) -> std::string
     return std::format("r={}, g={}, b={}, a={}", color.r, color.g, color.b, color.a);
 }
 
-}
+} // namespace pong
