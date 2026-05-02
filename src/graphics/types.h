0bf59fc91e05251dd22649cf7c990b14d8f05796
Andy Miller <armiller76@gmail.com>
2026-05-02 08:40:28 -0400

    Extend to provide type conversion helpers

--- src/graphics/types.h
+++ src/graphics/types.h
@@ -2,6 +2,10 @@
 
 #include <string>
 
+#include <vulkan/vulkan.hpp>
+
+#include "utils/exception.h"
+
 namespace pong
 {
 
@@ -12,20 +16,16 @@ enum class AlphaMode
     Blend,
 };
 
-enum class MagFilterMode
+enum class FilterMode
 {
     Nearest,
     Linear,
 };
 
-enum class MinFilterMode
+enum class MipMapMode
 {
     Nearest,
     Linear,
-    NearestMipmapNearest,
-    LinearMipmapNearest,
-    NearestMipmapLinear,
-    LinearMipmapLinear,
 };
 
 enum class WrapMode
@@ -35,6 +35,17 @@ enum class WrapMode
     ClampToEdge,
 };
 
+enum class CompareOp
+{
+    Never,
+    Equal,
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Always,
+};
+
 constexpr auto to_string(AlphaMode alpha_mode) -> std::string
 {
     switch (alpha_mode)
@@ -42,6 +53,52 @@ constexpr auto to_string(AlphaMode alpha_mode) -> std::string
         case AlphaMode::Blend: return "blend";
         case AlphaMode::Mask: return "mask";
         case AlphaMode::Opaque: return "opaque";
+    }
+}
+
+constexpr auto to_vk(FilterMode m) -> ::vk::Filter
+{
+    switch (m)
+    {
+        case FilterMode::Linear: return ::vk::Filter::eLinear;
+        case FilterMode::Nearest: return ::vk::Filter::eNearest;
+        default: throw arm::Exception("invalid filter mode");
+    }
+}
+
+constexpr auto to_vk(MipMapMode m) -> ::vk::SamplerMipmapMode
+{
+    switch (m)
+    {
+        case MipMapMode::Linear: return ::vk::SamplerMipmapMode::eLinear;
+        case MipMapMode::Nearest: return ::vk::SamplerMipmapMode::eNearest;
+        default: throw arm::Exception("invalid filter mode");
+    }
+}
+
+constexpr auto to_vk(WrapMode m) -> ::vk::SamplerAddressMode
+{
+    switch (m)
+    {
+        case WrapMode::ClampToEdge: return ::vk::SamplerAddressMode::eClampToEdge;
+        case WrapMode::Repeat: return ::vk::SamplerAddressMode::eRepeat;
+        case WrapMode::MirroredRepeat: return ::vk::SamplerAddressMode::eMirroredRepeat;
+        default: throw arm::Exception("invalid sampler wrap/address mode");
+    }
+}
+
+constexpr auto to_vk(CompareOp o) -> ::vk::CompareOp
+{
+    switch (o)
+    {
+        case CompareOp::Never: return ::vk::CompareOp::eNever;
+        case CompareOp::Always: return ::vk::CompareOp::eAlways;
+        case CompareOp::Equal: return ::vk::CompareOp::eEqual;
+        case CompareOp::Less: return ::vk::CompareOp::eLess;
+        case CompareOp::LessOrEqual: return ::vk::CompareOp::eLessOrEqual;
+        case CompareOp::Greater: return ::vk::CompareOp::eGreater;
+        case CompareOp::GreaterOrEqual: return ::vk::CompareOp::eGreaterOrEqual;
+        default: throw arm::Exception("invalid compare op");
     }
 }
 
