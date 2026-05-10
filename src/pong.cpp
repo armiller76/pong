0c36dc0455b476dd13af455f248adc6fd498302b
Andy Miller <armiller76@gmail.com>
2026-05-09 20:23:08 -0400

    Integrate refactors and debug code

--- src/pong.cpp
+++ src/pong.cpp
@@ -1,11 +1,10 @@
-#include <cstdlib>
 #include <string_view>
 
 #include <vulkan/vulkan_raii.hpp>
 
 #include "engine/engine_types.h"
+#include "engine/input_state.h"
 #include "engine/render_context.h"
-#include "engine/vulkan/vulkan_renderer.h"
 #include "platform/win32_window.h"
 #include "utils/exception.h"
 #include "utils/log.h"
@@ -28,8 +27,12 @@ int main()
             .version = {.major = APP_VERSION_MAJOR, .minor = APP_VERSION_MINOR, .patch = APP_VERSION_PATCH}};
 
         // TODO: other platforms
-        auto win32_window = pong::Win32Window(render_context_info);
-        auto render_context = pong::RenderContext(render_context_info, win32_window);
+        auto input_state = pong::InputState();
+        auto win32_window = pong::Win32Window(render_context_info, input_state);
+        auto render_context = pong::RenderContext(render_context_info, win32_window, input_state);
+
+        // auto scene = render_context.load_scene("assets/gltf/CesiumMilkTruck/CesiumMilkTruck.glb");
+        // auto scene = render_context.load_scene("assets/gltf/BoomBox/BoomBox.glb");
 
         auto scene = render_context.load_scene("assets/gltf/DamagedHelmet/DamagedHelmet.glb");
         scene.entities().at(scene.root_indices().at(0).value).scale_by({2.0f, 2.0f, 2.0f});
@@ -37,12 +40,10 @@ int main()
         scene.add_directional_light({{0.6f, -0.5f, 0.4f, 0.4f}, {0.6f, 0.7f, 1.0f, 0.0f}});
         scene.add_directional_light({{0.1f, -0.3f, 1.0f, 0.6f}, {0.9f, 0.9f, 1.0f, 0.0f}});
 
-        // auto scene = render_context.load_scene("assets/gltf/CesiumMilkTruck/CesiumMilkTruck.glb");
-        //  auto scene = vk_renderer.load_scene("assets/gltf/BoomBox/BoomBox.glb");
-
         while (!win32_window.should_close())
         {
             win32_window.process_events();
+            input_state.process_events();
 
             // accum_time += delta.count();
             // auto angle_x = accum_time * 0.00025f;
@@ -56,16 +57,16 @@ int main()
             render_context.update_and_render(scene);
         }
         render_context.shutdown();
-        return EXIT_SUCCESS;
+        return 0;
     }
     catch (arm::Exception &e)
     {
         arm::log::error("Pong error: {}", e.to_string());
-        return EXIT_FAILURE;
+        return 1;
     }
     catch (...)
     {
         arm::log::error("Unknown error, exiting...");
-        return EXIT_FAILURE;
+        return 1;
     }
 } // main()
