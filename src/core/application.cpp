6315be7b18e4119dfb9cf4c3ff75c9537ffeb34f
Andy Miller <armiller76@gmail.com>
2026-06-22 16:51:45 -0400

    Add Application class

---
+++ src/core/application.cpp
@@ -0,0 +1,62 @@
+#include "core/application.h"
+
+#include <memory>
+
+#include "imgui/imgui_wrapper.h"
+
+#include "core/scene.h"
+#include "engine/input_state.h"
+#include "engine/render_context.h"
+#include "platform/win32_window.h"
+#include "utils/log.h"
+
+namespace pong
+{
+
+Application::Application(ApplicationInfo info)
+    : app_info_{std::move(info)}
+    , render_info_{RenderContextInfo{
+          .frames_in_flight = 2u,
+          .clear_color = {},
+          .window_rect = {},
+      }}
+    , input_state_{}
+    , win32_window_{app_info_.application_name, render_info_, input_state_}
+    , vulkan_instance_{app_info_}
+    , render_context_{render_info_, win32_window_, input_state_, vulkan_instance_}
+    , debug_renderer_{nullptr}
+{
+    arm::log::debug("Application constructor");
+
+    debug_renderer_ = std::make_unique<ImguiWrapper>(
+        win32_window_.win32_handles().window,
+        vulkan_instance_,
+        render_context_,
+        app_info_.application_root_dir.string());
+    render_context_.init_debug_renderer(debug_renderer_.get());
+}
+
+Application::~Application() = default;
+
+auto Application::run() -> void
+{
+
+    // auto scene = render_context.load_scene("assets/gltf/CesiumMilkTruck/CesiumMilkTruck.glb");
+    // auto scene = render_context.load_scene("assets/gltf/BoomBox/BoomBox.glb");
+    auto scene = render_context_.load_scene("assets/gltf/DamagedHelmet/DamagedHelmet.glb");
+    scene.entities().at(scene.root_indices().at(0).value).scale_by({2.0f, 2.0f, 2.0f});
+    scene.add_directional_light(
+        {.direction_intensity = {-0.5f, -1.0f, -0.3f, 1.5f}, .color = {1.0f, 0.95f, 0.8f, 0.0f}});
+    scene.add_directional_light({.direction_intensity = {0.6f, -0.5f, 0.4f, 0.4f}, .color = {0.6f, 0.7f, 1.0f, 0.0f}});
+    scene.add_directional_light({.direction_intensity = {0.1f, -0.3f, 1.0f, 0.6f}, .color = {0.9f, 0.9f, 1.0f, 0.0f}});
+
+    while (!win32_window_.should_close())
+    {
+        win32_window_.process_events();
+        input_state_.process_events();
+        render_context_.update_and_render(scene);
+    }
+    render_context_.shutdown();
+}
+
+} // namespace pong
---
+++ src/core/application.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+#include <string>
+
+#include "engine/input_state.h"
+#include "engine/render_context.h"
+#include "engine/vulkan/vulkan_instance.h"
+#include "platform/win32_window.h"
+
+namespace pong
+{
+
+class ImguiWrapper;
+
+struct Version
+{
+    std::uint32_t major;
+    std::uint32_t minor;
+    std::uint32_t patch;
+};
+
+struct ApplicationInfo
+{
+    std::string application_name;
+    std::string engine_name;
+    std::filesystem::path application_root_dir;
+    Version version;
+};
+
+class Application
+{
+  public:
+    Application(ApplicationInfo info);
+    ~Application();
+
+    auto run() -> void;
+
+  private:
+    ApplicationInfo app_info_;
+    RenderContextInfo render_info_;
+
+    InputState input_state_;
+    Win32Window win32_window_;
+    VulkanInstance vulkan_instance_;
+    RenderContext render_context_;
+
+    std::unique_ptr<ImguiWrapper> debug_renderer_;
+};
+
+}
