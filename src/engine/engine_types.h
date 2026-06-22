cc5a3c41f9580101625f87b0a4e1aedc0cd7b7c4
Andy Miller <armiller76@gmail.com>
2026-06-22 16:55:07 -0400

    Move RenderInfo struct back to RenderContext

--- src/engine/engine_types.h
+++ src/engine/engine_types.h
@@ -2,10 +2,6 @@
 
 #include <cstdint>
 #include <string>
-#include <string_view>
-
-#include "graphics/color.h"
-#include "math/rectangle.h"
 
 namespace pong
 {
@@ -15,17 +11,6 @@ struct Version
     std::uint32_t major;
     std::uint32_t minor;
     std::uint32_t patch;
-};
-
-struct RenderContextInfo
-{
-    std::string_view project_root;
-    std::string_view app_name;
-    std::string_view engine_name;
-    std::uint32_t frames_in_flight;
-    pong::Color clear_color;
-    pong::Rectangle window_rect;
-    Version version;
 };
 
 enum class ResultCode
